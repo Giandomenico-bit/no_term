4269c871220846025ba04dda0bef3dbc1481f052
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-11-01 00:24:11 +0100

    TheSum.c

---
+++ TheSum.c
@@ -0,0 +1,42 @@
+/* Si scriva un programma C che, dopo aver chiesto all'utente di
+inserire un numero intero da tastiera, lo sommi ad un altro intero
+letto da un file di testo sum.dat. Infine aggiorni il file sum.dat
+con il nuovo valore. Il programma tenga conto del caso in cui
+il file sum.dat non esista. */
+
+#include <stdio.h>
+
+int main( void ){
+
+  int value;
+  int valueInFile;
+  int sum;
+  FILE* stream;
+
+  puts( "Lettura dal file sum.dat in corso..." );
+
+  stream = fopen( "sum.dat", "r" );
+  if( !stream ) puts( "File non trovato..!\n" );
+  else{
+    fscanf( stream, "%d", &valueInFile );
+    printf( "Valore corrente : %d\n\n", valueInFile );
+
+    printf( "Inserisci valore da sommare : " );
+    scanf( "%d", &value );
+    puts( "" );
+    fclose( stream );
+
+    stream = fopen( "sum.dat", "w" );
+    if( !stream ) puts( "Impossibile aprire il file..!\n" );
+    else{
+      sum = value + valueInFile;
+      fprintf( stream, "%d", sum );
+
+      printf( "Nuovo valore : %d\n", sum );
+      printf( "Salvataggio nel file sum.dat in corso...\n" );
+      fclose( stream );
+    }
+  }
+
+  return 0;
+}
